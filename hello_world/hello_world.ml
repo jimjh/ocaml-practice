4ec7c070924907ab4ee743ceacc06925eafc4738
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-05-22 23:11:03 -0400

    added recursion example

--- hello_world/hello_world.ml
+++ hello_world/hello_world.ml
@@ -1,1 +1,13 @@
-print_string "Hello World"
+print_string "Hello World";
+
+(* This is a comment *)
+
+type row = int list
+type students = row array
+
+(* Purpose: add up the numbers in the row *)
+let sum (r : row) : int =
+  let rec aux n = function
+    | [] -> n
+    | h::t ->  aux (h + n) t
+  in aux 0 r;;
